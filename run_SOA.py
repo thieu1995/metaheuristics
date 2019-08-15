c86fa1f153bf6b86c65a968a9318b2ca56c2b17c
ThieuNv <nguyenthieu2102@gmail.com>
2019-08-15 18:12:32 +0700

    Sandpaper optimization algorith - SOA - DONE

--- run_SOA.py
+++ run_SOA.py
@@ -1,17 +1,19 @@
-from models.multiple_solution.physics_based.TWO import BaseTWO, OppoTWO, OTWO, LevyTWO, ITWO
+from models.multiple_solution.swarm_based.SOA import BaseSOA, OriginalSOA
 from utils.FunctionUtil import *
 
+## Setting parameters
 root_paras = {
     "problem_size": 30,
     "domain_range": [-1, 1],
     "print_train": True,
-    "objective_func": C11
+    "objective_func": C1
 }
-two_paras = {
+soa_paras = {
     "epoch": 1000,
     "pop_size": 100,
 }
 
 ## Run model
-md = OppoTWO(root_algo_paras=root_paras, two_paras=two_paras)
+md = BaseSOA(root_algo_paras=root_paras, soa_paras=soa_paras)
 md._train__()
+
