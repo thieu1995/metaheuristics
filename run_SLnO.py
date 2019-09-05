805e2a033c334c4b039e77e00756b7e531e35903
trung02012017 <trungbka1997@gmail.com>
2019-09-05 10:16:35 +0700

    fix to run hybrid + compos

--- run_SLnO.py
+++ run_SLnO.py
@@ -3,9 +3,7 @@ from utils.FunctionUtil import *
 import json
 import os
 
-function_list = [islo_uni_F1, islo_uni_F2, islo_uni_F3, islo_uni_F4, islo_uni_F5, islo_uni_F6, islo_uni_F7, islo_uni_F8,
-                 islo_multi_F9, islo_multi_F10, islo_multi_F11, islo_multi_F12, islo_multi_F13, islo_multi_F14,
-                 islo_multi_F15, islo_multi_F16, islo_hybrid_F17, islo_hybrid_F18, islo_hybrid_F19, islo_hybrid_F20,
+function_list = [ islo_hybrid_F17, islo_hybrid_F18, islo_hybrid_F19, islo_hybrid_F20,
                  islo_hybrid_F21, islo_hybrid_F22, islo_hybrid_F23, islo_compos_F24, islo_compos_F25, islo_compos_F26,
                  islo_compos_F27, islo_compos_F28, islo_compos_F29, islo_compos_F30]
 
@@ -60,7 +58,7 @@ for dim in dimensions:
 path = "results/"+model_name
 if not os.path.exists(path):
     os.makedirs(path)
-with open(path+'/'+model_name+'.json', 'w') as fp:
+with open(path+'/'+model_name+'_2.json', 'w') as fp:
     json.dump(results, fp)
 
 # ## Setting parameters`
