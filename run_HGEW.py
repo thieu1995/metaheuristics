e3ee66c6d6591ba41885b53d1f164718b89b9911
Thieu Nguyen <nguyenthieu2102@gmail.com>
2020-02-18 01:42:58 +0000

    results 50D

--- run_HGEW.py
+++ run_HGEW.py
@@ -7,7 +7,7 @@ function_list = [C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, C13, C14, C1
                  C23, C24, C25, C26, C27, C28, C29, C30]
 
 # function_list = [islo_uni_F1]
-dimensions = [100]
+dimensions = [50]
 GSO_paras = {
         "epoch": [50, 10, 300],
         "pop_size": 150,
