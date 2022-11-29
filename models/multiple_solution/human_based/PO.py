196e591e990a1748bb439747685d4d03713d6b50
ugomuhieddine17 <72077493+ugomuhieddine17@users.noreply.github.com>
2022-11-29 22:52:51 +0100

    adaptation improvement to mealpy library

--- models/multiple_solution/human_based/PO.py
+++ models/multiple_solution/human_based/PO.py
@@ -1,18 +1,30 @@
 import numpy as np
 
-
-
 class PO:
-    def __init__(self):
-        self.fun = None
-        self.lambdamax = 0
-        self.n = 0
-        self.tmax = 0
-        self.d = 0
+    def __init__(self, po_params):
+        '''
+        Parameters:
+        fun -> the function we want to optimize
+        n -> the number of contituencies, political parties and party members
+        lambdamax -> upper limit of the party switching rate
+        tmax -> number of iterations
 
-    def ElectionCampaign(self, fun, d, pji, pjitminus1, pstari, cstarj):
-        if (fun(pji) <= fun(pjitminus1)):
-            for k in range(d):
+        d -> number of dimensions, we don't want this to be a parameter later I think
+
+        Out: the final population
+
+        '''
+        self.fun = po_params['fun']
+        self.lambdamax = po_params['lambdamax']
+        self.n = po_params['n']
+        self.tmax = po_params['tmax']
+        self.lambdamax = po_params['lambdamax']
+        self.d = po_params['d']
+
+        
+    def ElectionCampaign(self, pji, pjitminus1, pstari, cstarj):
+        if (self.fun(pji) <= self.fun(pjitminus1)):
+            for k in range(self.d):
                 mstar = pstari[k]
                 r = np.random.rand()
                 # Implementation of eq9
@@ -32,7 +44,7 @@ class PO:
                 elif ((mstar <= pjitminus1[k] <= pji[k]) or (mstar >= pjitminus1[k] >= pji[k])):
                     pji[k] = mstar + (2 * r - 1) * abs(mstar - pjitminus1[k])
         else:
-            for k in range(d):
+            for k in range(self.d):
                 mstar = pstari[k]
                 r = np.random.rand()
                 # Implementation of eq 10
@@ -53,14 +65,14 @@ class PO:
                     pji[k] = mstar + (2 * r - 1) * abs(mstar - pjitminus1[k])
         return pji
 
-    def PartySwitching(self, parties, population, Lambda, fitnesst, n):
+    def PartySwitching(self, parties, population, Lambda, fitnesst):
         for i in range(len(parties)):
             # got the ith party
             for j in range(len(parties[i])):
                 # for each member of the party
                 sp = np.random.rand()
                 if (sp < Lambda):
-                    r = np.random.randint(0, n - 1)  # the random new party (from 0 to n-1 as it how python indexes)
+                    r = np.random.randint(0, self.n - 1)  # the random new party (from 0 to n-1 as it how python indexes)
                     q = np.argmax(fitnesst[parties[r]])  # do we have to recompute here? or time consuming
                     old = parties[i][j].copy()
                     new = parties[r][q].copy()
@@ -77,45 +89,28 @@ class PO:
                     # swap pqr and pji
         return parties
 
-    def ParliamentaryAffairs(self,constituencyLeaders, population, n, fun):
-        for j in range(n):
-            r = np.random.randint(0, n - 1)
+    def ParliamentaryAffairs(self, constituencyLeaders, population):
+        for j in range(self.n):
+            r = np.random.randint(0, self.n - 1)
             # to make sure that r != j
             while r == j:
-                r = np.random.randint(0, n - 1)
+                r = np.random.randint(0, self.n - 1)
 
             a = np.random.rand()
             c_j = population[int(constituencyLeaders[j])]
             c_r = population[int(constituencyLeaders[r])]
             c_new = c_r + (2 * a - 1) * np.linalg.norm(c_r - c_j)
 
-            print(c_new)
-            fitness_new = fun(c_new)
-            fitness_j = fun(c_j)
+            #print(c_new)
+            fitness_new = self.fun(c_new)
+            fitness_j = self.fun(c_j)
             if fitness_new <= fitness_j:
                 population[int(constituencyLeaders[j])] = c_new
 
         return 0
 
-    def PoliticalOptimizer(self, fun, n, lambdamax, tmax, d):
-        '''
-        Parameters:
-        fun -> the function we want to optimize
-        n -> the number of contituencies, political parties and party members
-        lambdamax -> upper limit of the party switching rate
-        tmax -> number of iterations
-
-        d -> number of dimensions, we don't want this to be a parameter later I think
-
-        Out: the final population
-
-        '''
-        self.fun = fun
-        self.n = n
-        self.lambdamax = lambdamax
-        self.tmax = tmax
-        self.d = d
-
+    def PoliticalOptimizer(self):
+       
         ################
         # Initialization: sth arbitrary now to replicate the paper, making 9 2D individuals
         # n = 3 parties, d = 2 dimensions
@@ -124,7 +119,7 @@ class PO:
         populationt = np.zeros((self.n ** 2, self.d))
         for i in range(self.n ** 2):
             populationt[i, :] = np.random.normal(0, 1, self.d)
-        print(populationt)
+        #print(populationt)
         ###############
         # Initializing parties
         # We have n parties and constituencies in the political system, we initialize them here
@@ -132,13 +127,13 @@ class PO:
         # we store the indices in parties, so that we can always use the indices to refer to individuals in the population
         ###############
         parties = constituencies = np.array([range(self.n * i, (self.n * i + self.n)) for i in range(self.n)])
-        print(f'The parties: {parties}')
+        #print(f'The parties: {parties}')
 
         ###############
         # Initialize the fitness of all individuals
         ###############
-        fitnesst = np.array([fun(x) for x in populationt])
-        print(f'Their fitness: {fitnesst}')
+        fitnesst = np.array([self.fun(x) for x in populationt])
+        #print(f'Their fitness: {fitnesst}')
         #################
         # Computing the party leaders
         #################
@@ -152,7 +147,7 @@ class PO:
             # 0 * 3 + 0,1 or 2. Then 1*3 + 0,1,2
             n_party += 1
 
-        print(f'The selected party leaders: {party_leaders}')
+        #print(f'The selected party leaders: {party_leaders}')
 
         ################
         # Computing the constituency leaders, 1 member for each party competes against each other
@@ -177,7 +172,7 @@ class PO:
             constituency_leaders[n_constituency] = n_constituency + 3 * np.argmin(fitnesst[constituency])
             n_constituency += 1
 
-        print(f'The constituency leaders: {constituency_leaders}')
+        #print(f'The constituency leaders: {constituency_leaders}')
 
         ################
         # Further initialisations
@@ -185,13 +180,14 @@ class PO:
         t = 1
         populationtmin1 = populationt.copy()
         fitnesstmin1 = fitnesst.copy()
-        Lambda = lambdamax
-        print(f'index of p00 is: {parties[0][0]}')
-        print(f'individual p00 is: {populationt[parties[0, 0]]}')
+        Lambda = self.lambdamax
+        #print(f'index of p00 is: {parties[0][0]}')
+        #print(f'individual p00 is: {populationt[parties[0, 0]]}')
         ################
         # Starting while loop
         ################
-        while (t <= tmax):
+        self.df = pd.DataFrame()
+        while (t <= self.tmax):
             populationtemp = populationt.copy()
 
             fitnesstemp = fitnesst.copy()
@@ -199,16 +195,16 @@ class PO:
                 for j in range(len(party)):
                     # partyleaders and constituency leaders give the index of the party leader
                     # we can plug this into the population to get the party leader
-                    populationt[parties[i, j]] = self.ElectionCampaign(fun, d, pji=populationt[parties[i, j]].copy(),
+                    populationt[parties[i, j]] = self.ElectionCampaign(pji=populationt[parties[i, j]].copy(),
                                                                   pjitminus1=populationtmin1[parties[i, j]].copy(),
                                                                   pstari=populationt[int(party_leaders[i])].copy(),
                                                                   cstarj=populationt[
                                                                       int(constituency_leaders[j])].copy())
 
-            parties = self.PartySwitching(parties, populationt, Lambda, fitnesst, n)
+            parties = self.PartySwitching(parties, populationt, Lambda, fitnesst)
 
             # fitness of each party member pji
-            fitnesstemp = np.array([fun(x) for x in populationtemp])
+            fitnesstemp = np.array([self.fun(x) for x in populationtemp])
 
             # finding party leaders
             n_party = 0
@@ -224,12 +220,11 @@ class PO:
                 constituency_leaders_loop[n_constituency] = n_constituency + 3 * np.argmin(fitnesst[constituency])
                 n_constituency += 1
 
-            self.ParliamentaryAffairs(constituency_leaders_loop, populationt, self.n, fun);
+            self.ParliamentaryAffairs(constituency_leaders_loop, populationt);
 
-            populationtmin1 = populationtemp
-            fitnesstmin1 = fitnesstemp
-            Lambda = Lambda - float(lambdamax) / tmax
+            self.populationtmin1 = populationtemp
+            self.fitnesstmin1 = fitnesstemp
+            self.df[str(t)] = list(self.fitnesstmin1)
+            Lambda = Lambda - float(self.lambdamax) / self.tmax
             t += 1
-
-po = PO()
-po.PoliticalOptimizer(lambda x: x[0]**2 + x[1]**2,3,2,2,2)
+        return self.populationtmin1[np.argmin(self.fitnesstmin1)], np.min(self.fitnesstmin1)
